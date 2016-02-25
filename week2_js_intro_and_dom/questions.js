f84c226da4fd1cca2ce948cdff77d328dc1aabbe
Parker Lewis <parker.lewis14@att.net>
2016-02-25 11:48:08 -0800

    adding solution

--- week2_js_intro_and_dom/questions.js
+++ week2_js_intro_and_dom/questions.js
@@ -1,75 +1,123 @@
+// IGNORE THIS!!
+var classroom = {
+    instructors: [
+      {
+        name: "Parker",
+        favTopic: ["hoisting", "merge conflicts"]
+      },
+      {
+        name: "Janey",
+        favTopic: ["angular", "hoisting"]
+      },
+      {
+        name: "Elie",
+        favTopic: ["jQuery", "Node"]
+      },
+      {
+        name: "Matt",
+        favTopic: ["DOM", "Math"]
+      },
+      {
+        name: "Tim",
+        favTopic: ["Computer Science", "Databases"]
+      },
+    ]
+  };
+
 module.exports = {
 
-  // fill in this array with all of the primitive types in JavaScript make these ALL a string (there are 5 we are looking for)
-  allPrimitiveDataTypes: [],
+  // fill in this array with all of the primitive types in JavaScript make these ALL a string (there are 5 we are looking for - Symbol is not one of them!)
+  allPrimitiveDataTypes: ["Boolean", "number", "null", "undefined", "string"],
 
-  // fill in all the falsey values in this array (there are 6)
-  allFalseyValues: [],
+  // fill in all the falsey values in this array (we gave you one to start!)
+  allFalseyValues: ["NaN","",false,undefined,null,0],
 
   // fill in four common JS Errors we learned about in class
-  fourCommonJSErrors: [],
+  fourCommonJSErrors: ["SyntaxError", "ReferenceError", "TypeError", "RangeError"],
 
   // Fix this broken function
 
   add: function(a,b){
-    a + b;
+    return a + b;
   },
 
   // fix this broken code
   sumFromZeroToFive: function(){
     var count =0;
-    for(var i=0; i > 5; i++){
+    for(var i=0; i < 5; i++){
       count += i;
     }
     return count;
   },
   // fix this broken code
   simpleComparison: function(a,b){
-    if (a=b) {
+    if (a===b) {
       return true;
     }
   },
 
   // Nested Data Structures
 
-  classroom: {
-    instructors: [
-    {
-      name: "Parker",
-      favTopic: ["hoisting", "merge conflicts"]
-    },
-    {
-      name: "Janey",
-      favTopic: ["angular", "hoisting"]
-    },
-    {
-      name: "Elie",
-      favTopic: ["jQuery", "Node"]
-    },
-    {
-      name: "Matt",
-      favTopic: ["DOM", "Math"]
-    },
-    {
-      name: "Tim",
-      favTopic: ["Computer Science", "Databases"]
-    },
-    ]
-  },
+  // classroom: {
+  //   instructors: [
+  //   {
+  //     name: "Parker",
+  //     favTopic: ["hoisting", "merge conflicts"]
+  //   },
+  //   {
+  //     name: "Janey",
+  //     favTopic: ["angular", "hoisting"]
+  //   },
+  //   {
+  //     name: "Elie",
+  //     favTopic: ["jQuery", "Node"]
+  //   },
+  //   {
+  //     name: "Matt",
+  //     favTopic: ["DOM", "Math"]
+  //   },
+  //   {
+  //     name: "Tim",
+  //     favTopic: ["Computer Science", "Databases"]
+  //   },
+  //   ]
+  // },
 
   // write a function that returns an array of all the names of the instructors in the classroom
   returnAllNames: function(){
+    var names = [];
+
+    for(var i = 0; i < classroom.instructors.length; i++) {
+      names.push(classroom.instructors[i].name);
+    }
+
+    return names;
 
   },
 
-  // Write the code necessary to print out the total number of favorite topics in the nested data structure
+  // Write the code necessary to return the total number of favorite topics in the nested data structure
   returnAllFavoriteTopics: function(){
-
+    var counter = 0;
+    for(var i = 0; i < classroom.instructors.length; i++){
+      for(var j = 0; j < classroom.instructors[i].favTopic.length; j++) {
+        counter++;
+      }
+    }
+    return counter;
   },
 
   // Write a function that accepts as arguments the name of an instructor and a favorite topic and adds that favorite topic to the instructor's favTopic array. This function should return that instructors favTopic array with the new favTopic. Do NOT worry about duplicates!
 
-  addFavoriteTopic: function(){
+  addFavoriteTopic: function(instructor, newFavTopic){
+    // classroom.instructors[instructors]
+    for(var i = 0; i < classroom.instructors.length; i++) {
+      if(classroom.instructors[i].name === instructor) {
+        classroom.instructors[i].favTopic.push(newFavTopic);
+        return classroom.instructors[i].favTopic;
+      }
+    }
+
+
 
   },
 
@@ -83,7 +131,19 @@ module.exports = {
       // o: 1
   // }
   // The order that these keys are returned in does NOT matter!
-
+  
+  letterCount: function(word){
+    var answer = {};
+    for(var i=0; i<word.length;i++){
+      if(answer[word[i]]){
+        answer[word[i]]++;
+      }
+      else {
+        answer[word[i]] =1;
+      }
+    }
+    return answer;
+  }
 };
 
 
--- week2_js_intro_and_dom/test/week2AssessmentSpec.js
+++ week2_js_intro_and_dom/test/week2AssessmentSpec.js
@@ -1,18 +1,78 @@
 var expect = require("chai").expect;
 var questions = require("../questions");
 
-describe("Student answers", function(){
-  describe("#letterCount", function(){
-    it("It should return the correct result", function(){
-      expect(questions.letterCount("awesome")).to.deep.equal({
-        a: 1,
-        w: 1,
-        e: 2,
-        s: 1,
-        m: 1,
-        o: 1
-       });
-    });
+describe("allPrimitiveDataTypes", function(){
+  it("includes all the primitive data types in JavaScript", function(){
+    // this next line just takes your answer and returns a new array with everything to lowerCase
+    var primitives = questions.allPrimitiveDataTypes.map(v => v.toLowerCase());
+    expect(primitives).to.have.members(["null", "undefined", "string", "boolean", "number"]);
   });
+});
 
+describe("allFalseyValues", function(){
+  it("includes all the falsey values in JavaScript", function(){
+    expect(questions.allFalseyValues).to.have.members(["",null,false,0,undefined,"NaN"]);
+  });
+});
+
+describe("fourCommonJSErrors", function(){
+  it("includes four common JS Errors ", function(){
+    // this next line just takes your answer and returns a new array with everything to lowerCase
+    var errors = questions.fourCommonJSErrors.map(v => v.toLowerCase());
+    expect(errors).to.have.members(["rangeerror", "typeerror", "syntaxerror", "referenceerror"]);
+  });
+});
+
+describe("#add", function(){
+  it("returns the sum of two arguments", function(){
+    expect(questions.add(2,3)).to.equal(5);
+  });
+});
+
+describe("#sumFromZeroToFive", function(){
+  it("adds all numbers from 0 to 5", function(){
+    expect(questions.sumFromZeroToFive()).to.equal(10);
+  });
+});
+
+describe("#simpleComparison", function(){
+  it("returns undefined if both arguments are of different type", function(){
+    expect(questions.simpleComparison(2,"2")).to.be.undefined;
+  });
+  it("returns true if both arguments are of the same type", function(){
+    expect(questions.simpleComparison(2,2)).to.be.true;
+  
+  });
+});
+
+describe("#returnAllNames", function(){
+  it("returns all names of instructors in an array", function(){
+    var names = questions.returnAllNames().map(v => v.toLowerCase());
+    expect(names).to.have.members(["parker", "elie", "janey", "matt", "tim"]);
+  });
+});
+
+describe("#returnAllFavoriteTopics", function(){
+  it("returns the number of favorite topics of all instructors", function(){
+    expect(questions.returnAllFavoriteTopics()).to.equal(10);
+  });
+});
+
+describe("#addFavoriteTopic", function(){
+  it("return an array with the given favorite topic of the given instructor added", function(){
+    expect(questions.addFavoriteTopic("Tim", "sailing")).to.deep.equal(["Computer Science", "Databases", "sailing"]);
+  });
+});
+
+describe("#letterCount", function(){
+  it("returns the correct frequency of letters in a string", function(){
+    expect(questions.letterCount("awesome")).to.deep.equal({
+      a: 1,
+      w: 1,
+      e: 2,
+      s: 1,
+      m: 1,
+      o: 1
+     });
+  });
 });
