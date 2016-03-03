33f838d1b423016aaec8bbe646afa7e33ea3a2a1
Parker Lewis <parker.lewis14@att.net>
2016-03-03 11:45:40 -0800

    added descriptions

--- week3_js_iterators_recursion_oop/questions.js
+++ week3_js_iterators_recursion_oop/questions.js
@@ -1,30 +1,48 @@
-function drop(){
+// Creates a slice of array with n elements dropped from the beginning.
 
+function drop(arr, n){
+	
 }
 
+// Converts all elements in array into a string separated by separator.
 function join(){
 
 }
 
+// Returns an array of values from a given obj and given key
 function pluck(){
 
 }
 
+// Repeats the given string n times.
 function repeat(){
 
 }
 
+// Returns a true or false depending on whether or not an input contains more than
+// one instance of a given character
 function containsMoreThanOne(){
 
 }
 
+// Same as last week but don't use a for in loop
 function letterCount(){
 
 }
 
+// Itterates over an array and flattens the element if it is an array
+// flatten([1, [2, [3, [4]], 5]]);
+// → [1, 2, [3, [4]], 5]
+
 function flatten(){
 
 }
+
+// BONUS
+
+// Flattens (or reduces...) all arrays of arrays to a single array of values
+// flattenDeep([1, [2, [3, [4]], 5]]);
+// → [1, 2, 3, 4, 5]
 
 function flattenDeep(){
 
